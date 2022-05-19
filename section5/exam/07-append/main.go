e615f471c2c6b4ddda41bf99e08421cab7af760d
suslmk <min.lee@infranicsmail.com>
2022-05-20 06:06:00 +0900

    Solve problem 07

--- section5/exam/07-append/main.go
+++ section5/exam/07-append/main.go
@@ -8,6 +8,11 @@
 
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Append
 //
@@ -23,11 +28,17 @@ package main
 
 func main() {
 	// 1. uncomment the code below
-	// png, header := []byte{'P', 'N', 'G'}, []byte{}
+	png, header := []byte{'P', 'N', 'G'}, []byte{}
 
 	// 2. append elements to header to make it equal with the png slice
-
+	header = append(png)
 	// 3. compare the slices using the bytes.Equal function
+	rst := bytes.Equal(header, png)
 
 	// 4. print whether they're equal or not
+	fmt.Println(rst)
+
+	if bytes.Equal(png, header) {
+		fmt.Println("They are equal")
+	}
 }
